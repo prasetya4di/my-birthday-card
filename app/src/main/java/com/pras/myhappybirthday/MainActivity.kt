88d4f7de8a5ba1da7bd9804673b705d8a521a6a0
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-18 18:24:42 +0700

    Add modifier and content scale to image

--- app/src/main/java/com/pras/myhappybirthday/MainActivity.kt
+++ app/src/main/java/com/pras/myhappybirthday/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -41,7 +40,14 @@ class MainActivity : ComponentActivity() {
 fun BirthdayGreetingWithImage(message: String, from: String) {
     val image = painterResource(R.drawable.androidparty)
     Box {
-        Image(image, null)
+        Image(
+            image,
+            null,
+            Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
         BirthdayGreetingWithText(
             "Happy Birthday Pras !",
             "- from Elsa"
